e9c3ff20fab4f243c74bbf11c0fcf3e7d9e0f38c
johnchavez4 <john_chavez4@my.vcccd.edu>
2021-03-22 19:18:02 +0000

    second commit

--- quiz2-2-1.cpp
+++ quiz2-2-1.cpp
@@ -6,15 +6,15 @@ using namespace std;
 int main()
 {
   int i, random_num;
-  int num = 5;
 
   cout << "Here are five random integers: \n" << endl;
 
   srand(time(NULL));
 
-  for (i = 0; i < num; i++)
+  for (i = 0; i < 5; i++)
   {
     random_num = rand() % 100;
-    
+    cout << random_num << endl;
   }
+  
 }
