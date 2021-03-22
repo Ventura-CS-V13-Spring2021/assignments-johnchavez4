ea28bbf5748519e29e52192d1a90dd8b060eda72
johnchavez4 <john_chavez4@my.vcccd.edu>
2021-03-22 20:55:04 +0000

    second commit

--- quiz2-2-5.cpp
+++ quiz2-2-5.cpp
@@ -4,5 +4,26 @@ using namespace std;
 
 int main()
 {
-  
+  int start, end;
+  int i, j;
+
+  do
+  {
+    cout << "Enter two numbers to set your range as: ";
+    cin >> start >> end;
+    cout << "Here are the prime numbers within the range " << start << " to " << end << ":\n";
+  }
+  while (start >= end; || start < 2 || end < 0);
+
+  for (i = start; i <= end; i++)
+  {
+    if (i == 0 || i == 1)
+      continue;
+    
+    for (j = 2; j <= (i / 2); j++)
+      if (i % j == 0)
+        break;
+    if (j > (i / 2))
+      cout << i << " is a prime number." << endl;
+  }
 }
