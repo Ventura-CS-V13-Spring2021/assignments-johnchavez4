3ef7354ada0747e6856011712aabf584c16f138e
johnchavez4 <john_chavez4@my.vcccd.edu>
2021-03-22 21:33:43 +0000

    second commit

--- quiz2-2-71.cpp
+++ quiz2-2-71.cpp
@@ -7,7 +7,26 @@ using namespace std;
 int main()
 {
   ofstream ofs;
-  int employee_ID;
-  string name, department;
+  int i, employee, employee_ID;
+  string employee_name, department;
+  double salary;
+
+  cout << "Enter the number of employees: ";
+  cin >> employee;
+
+  for (i = 0, i < employee; i++)
+  {
+    cout << "Enter the employee ID: \n";
+    cin >> employee_ID;
+    cout << "Enter the employee name: \n";
+    cin >> employee_name;
+    cout << "Enter the department name: \n";
+    cin >> department;
+    cout << "Enter the salary: $\n";
+    cin >> salary;
+  }
+  
+
+  ofs.open("employee.txt");
   
 }
