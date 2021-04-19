5d151ae46aaa1bc77a4e8956dbfd9b868fa468f1
johnchavez4 <>
2021-04-19 16:37:45 +0000

    second commit

--- midterm1-1.cpp
+++ midterm1-1.cpp
@@ -9,4 +9,12 @@ int main()
   ofstream ofs;
   int score1, score2;
   string student_name;
+
+  ofs.open("students.txt");
+
+  if (student_name == "James")
+  {
+      score1 = 100, score2 = 100;
+      cout << "James\n100   100";
+  }
 }
