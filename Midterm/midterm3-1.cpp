eed77b0cbf06116157ca836bf8fa718974e53a16
johnchavez4 <>
2021-04-19 17:43:38 +0000

    fifth commit

---
+++ Midterm/midterm3-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int getRdum(int random_num, i, N)
+{
+  N = 3;
+  srand(time(NULL));
+  for (i = 0; )
+}
