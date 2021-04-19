4531156a25f6d61c686554e6194aef4715e5f084
johnchavez4 <>
2021-04-19 17:37:29 +0000

    fourth commit

---
+++ Midterm/midterm1-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  ifstream ifs;
+  int James_score1 = 100, James_score2 = 100;
+  int Orlando_score1 = 95, Orlando_score2 = 90;
+  int Tim_score1 = 100, Tim_score2 = 90;
+  int Maxim_score1 = 100, Maxim_score2 = 90;
+  int Will_score1 = 100, Will_score2 = 100;
+  int Jason_score1 = 100, Jason_score2 = 90;
+  int David_score1 = 90, David_score2 = 90;
+  int Bill_score1 = 100, Bill_score2 = 95;
+  int Carmen_score1 = 100, Carmen_score2 = 100;
+  int Chris_score1 = 100, Chris_score2 = 90;
+
+  cout << "James" << "  Score1:" << Jason_score1 << 
+
+
+  ifs.open("students.txt");
+  
+}
