ecf9e53a0bc974b573938bbc937d4f742073d0b7
johnchavez4 <>
2021-04-26 23:22:17 +0000

    second commit

--- Quiz5/quiz5-1.cpp
+++ Quiz5/quiz5-1.cpp
@@ -1,12 +1,18 @@
-#include    <iostream>
+#include <iostream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-  const int SIZE=10;
-  int     numbers[10] = {5, 10, 75, 80, 25, 55, 95, 70, 60, 15};
-  int     diff[10];
-  int     sum=0;
+    const int SIZE=10;
+    int     numbers[10] = {5, 10, 75, 80, 25, 55, 95, 70, 60, 15};
+    int     diff[10];
+    int     sum=0;
 
-  //*** Your code 
+    for (int i = 0; i < 10; i++)
+    {
+        cout << numbers[i] << endl;
+    }
 }
