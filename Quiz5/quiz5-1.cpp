3ca06d8c687a418360d9332691ed4c2737e889db
johnchavez4 <>
2021-04-26 23:39:08 +0000

    third commit

--- Quiz5/quiz5-1.cpp
+++ Quiz5/quiz5-1.cpp
@@ -4,15 +4,28 @@
 #include <algorithm>
 using namespace std;
 
+int findSum(int arr[], int n)
+{
+  int sum_array = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum_array += arr[i];
+  }
+ return sum_array;
+}
+
 int main()
 {
-    const int SIZE=10;
-    int     numbers[10] = {5, 10, 75, 80, 25, 55, 95, 70, 60, 15};
-    int     diff[10];
-    int     sum=0;
+  const int SIZE=10;
+  int     numbers[10] = {5, 10, 75, 80, 25, 55, 95, 70, 60, 15};
+  int     diff[10];
+  int     sum=0;
 
-    for (int i = 0; i < 10; i++)
-    {
-        cout << numbers[i] << endl;
-    }
+  for (int i = 0; i < 10; i++)
+  {
+    cout << numbers[i] << endl;
+  }
+  int n = sizeof(numbers) / sizeof(numbers[0]);
+  cout << "Sum = " << findSum(numbers, n);
+  return 0;
 }
