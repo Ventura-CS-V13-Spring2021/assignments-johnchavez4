13136d813e2586ff68349ef452321a551663c34b
johnchavez4 <>
2021-05-03 23:58:43 +0000

    2nd commit(put code in wrong file lol)

---
+++ Quiz6/IntegerArray.hpp
@@ -0,0 +1,19 @@
+#ifndef INTEGERARRAY_H
+#define INTEGERARRAY_H
+
+class IntegerArray
+{
+private:
+  static const int N = 10;
+  int numbers[N];
+  int length = 0;
+
+public:
+  int getLength(void) const;
+  void sortArray(int flag); // if flag is 0, ascending, otherwise descending
+  void fillUp(void);
+  void getPrimeNumber(void) const;
+  void printArray(void) const;
+};
+
+#endif
