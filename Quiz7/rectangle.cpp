ad282d2c2cf44bd28a17d1afd2846f9e9cb58631
johnchavez4 <>
2021-05-11 04:39:04 +0000

    4th commit

--- Quiz7/rectangle.cpp
+++ Quiz7/rectangle.cpp
@@ -1,3 +1,21 @@
 #include "rectangle.hpp"
 #include "coordinate.hpp"
 #include <iostream>
+
+Rectangle::Rectangle(): lb(), rt(), center(), area(0.0)
+{
+}
+
+Rectangle::Rectangle(Coordinate lbval, Coordinate rtval): lb(lbval), rt(rtval)
+{
+}
+
+Coordinate Rectangle::getLB() const
+{
+  return lb;
+}
+
+Coordinate Rectangle::getRT() const
+{
+  return rt;
+}
