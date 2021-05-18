709d09562ac7cf8fb41c9b8f32fb4ef5f8da906d
johnchavez4 <>
2021-05-18 17:17:14 +0000

    4th commit

--- Final/final2.cpp
+++ Final/final2.cpp
@@ -7,4 +7,21 @@ void count(int arr[], int length)
   std::vector<bool> visited(length, false); 
   
   std::cout << "N" << " " << "Count" << std::endl;
+  for (int i = 0; i < length; i++) 
+  { 
+    if (visited[i] == true)
+    { 
+      continue;
+    }
+    int count = 1; 
+    for (int j = i + 1; j < length; j++) 
+    { 
+      if (arr[i] == arr[j]) 
+      { 
+        visited[j] = true; 
+        count++; 
+      } 
+    }
+    std::cout << arr[i] << " " << count << std::endl; 
+  } 
 }
