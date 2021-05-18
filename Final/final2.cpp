f42f82abc0c96f4adc07553eaa5a37444c16385b
johnchavez4 <>
2021-05-18 17:09:36 +0000

    3rd commit, started on void function for finding # of entries for question 2

---
+++ Final/final2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void count(int arr[], int length) 
+{ 
+  std::vector<bool> visited(length, false); 
+  
+  std::cout << "N" << " " << "Count" << std::endl;
+}
